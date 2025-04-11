beba0199e4da4b59cd8597d02654d53709c9dbe1
Fitly <sharvin_mahendran@hotmail.com>
2025-04-11 19:23:32 +1000

    Update codemagic.yaml to specify a newer Flutter version and update_podspecs.sh to fix license paths

--- update_podspecs.sh
+++ update_podspecs.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 echo "Updating podspec files with absolute path references..."
 
-# Loop through all podspec files in plugin override folders (folders ending with _plugin)
+# Loop over all podspec files in directories ending with _plugin inside the ios folder.
 for podspec in ios/*_plugin/*.podspec; do
   if [ -f "$podspec" ]; then
     echo "Processing ${podspec}"
-    # Replace relative LICENSE reference with an absolute path
+    # Update the license file reference.
+    # This line replaces { :file => '../LICENSE' } with 
+    # { :file => File.expand_path('../../LICENSE', __FILE__) }.
     sed -i.bak "s/{ :file => '..\/LICENSE' }/{ :file => File.expand_path('..\/..\/LICENSE', __FILE__) }/g" "$podspec"
+    
+    # (Add additional sed commands here if needed.)
+    
+    # Remove the backup file created by sed.
     rm "$podspec.bak"
   fi
 done
